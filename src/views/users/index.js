b54cfed0d7916f0c2c5ac95ba4cf91b42ba5ac71
orayanics <nicole.oraya.cics@ust.edu.ph>
2025-02-03 20:34:53 +0800

    get users

--- src/views/users/index.js
+++ src/views/users/index.js
@@ -1,54 +1,61 @@
-import React from 'react';
-import {Container, Table, Button} from 'reactstrap';
+import React, { useEffect, useState } from "react";
+import { Container, Table, Button } from "reactstrap";
 
 function Index() {
-	return (
-		<Container>
-			<div className='mt-3 text-right'>
-				<Button color='primary'>+ Add User</Button>
-			</div>
+  const [users, setUsers] = useState([]);
 
-			<Table className='mt-3'>
-				<thead>
-					<tr>
-						<th>ID</th>
-						<th>Profile</th>
-						<th>Email</th>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th />
-					</tr>
-				</thead>
+  useEffect(() => {
+    // get all users 10 per page and get the other 2 users
 
-				<tbody>
-					<tr>
-						<th scope='row'>1</th>
-						<td />
-						<td />
-						<td>Mark</td>
-						<td>Otto</td>
-						<td />
-					</tr>
-					<tr>
-						<th scope='row'>2</th>
-						<td />
-						<td />
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td />
-					</tr>
-					<tr>
-						<th scope='row'>3</th>
-						<td />
-						<td />
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td />
-					</tr>
-				</tbody>
-			</Table>
-		</Container>
-	);
+    fetch("https://reqres.in/api/users?per_page=10")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUsers(data.data);
+      });
+  }, []);
+
+  return (
+    <Container>
+      <div className="mt-3 text-right">
+        <Button color="primary">+ Add User</Button>
+      </div>
+
+      <Table className="mt-3">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Profile</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Actions</th>
+            <th />
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <th scope="row">{user.id}</th>
+              <td>
+                <img src={user.avatar} alt={user.first_name} />
+              </td>
+              <td>{user.email}</td>
+              <td>{user.first_name}</td>
+              <td>{user.last_name}</td>
+              <td>
+                <div>
+                  <Button color="info">Edit</Button>{" "}
+                  <Button color="danger">Delete</Button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
 
 export default Index;
