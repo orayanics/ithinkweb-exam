a7764b9969f3b996910a44c774287ab64ff054b2
orayanics <nicole.oraya.cics@ust.edu.ph>
2025-02-04 20:34:04 +0800

    home

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -5,13 +5,23 @@ function Index() {
   return (
     <Container>
       <Card className="mt-5 p-5">
-        <h1 className="h1">Nicole E. Oraya</h1>
+        <p className="h1">Nicole Oraya</p>
+        <p className="h5">
+          I'm a <span className="gradient-text">web developer</span>. I design
+          and develop web apps for seamless user experience.
+        </p>
         <address>
-          <a className="text-decoration-none" href="mailto:orayanics@gmail.com">
+          <a
+            className="h6 text-decoration-none"
+            href="mailto:orayanics@gmail.com"
+          >
             orayanics@gmail.com
           </a>
           <br />
-          <a className="text-decoration-none" href="tel:+639567052824">
+          <a
+            className="h6 fw-normal text-decoration-none fst-italic text-secondary"
+            href="tel:+639567052824"
+          >
             (+63) 9567052824
           </a>
         </address>
